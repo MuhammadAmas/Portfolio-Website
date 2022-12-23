bc198afca64a2b2ebfbdb5ec44e5ef37948acc1b
MuhammadAmas <amaswaseem@gmail.com>
2022-12-23 19:15:49 +0500

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Header = props =>{
+
+    return <>
+
+        <div>Talal Ahmed</div>
+
+        <div className="headerLists">
+            <ul>
+                <li></li>
+                <li>Projects</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    
+    </>
+}
+
+export default Header;
