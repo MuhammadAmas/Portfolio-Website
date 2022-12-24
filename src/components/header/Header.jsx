f5015febcb9cfa3222aa8d00c41e4b092eb7e2f4
MuhammadAmas <amaswaseem@gmail.com>
2022-12-24 18:45:59 +0500

    Updated

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
-
+import classes from './Header.module.css';
 const Header = props =>{
 
     return <>
 
-        <div>Talal Ahmed</div>
+        <nav className={classes.navbar}>
 
-        <div className="headerLists">
-            <ul>
-                <li></li>
-                <li>Projects</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
+        <div className={classes.hamburger}>
+            <span className={classes.bar}></span>
+            <span className={classes.bar}></span>
+            <span className={classes.bar}></span>
         </div>
+        
+        <div className={classes.navLogo}>
+            <h2> Talal Ahmed</h2>
+        </div>
+        <ul className={classes.navMenu}>
+            <li className={classes.navItem}><a href="#my-projects">Projects</a></li>
+            <li className={classes.navItem}><a href="#about-me">About Me</a></li>
+            {/* <li className={classes.navItem}><a href="#my-skills">My Skills</a></li> */}
+            <li className={classes.navItem}><a href="#let's-connect">Let's Connect</a></li>
+        </ul>
+
+
+    </nav>
     
     </>
 }
